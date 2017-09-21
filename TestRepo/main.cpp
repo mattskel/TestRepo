e21e4275e2462288982b02567ca8023492e12b1f
mattskelley <matthew.skelley@tjm.com.au>
2017-09-21 13:36:12 +1000

    Commit changes to new-branch

--- TestRepo/main.cpp
+++ TestRepo/main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main() {
 	cout << "Hello World" << endl;
 	cout << "This is my first change to the project" << endl;
+	cout << "Created new-branch" << endl;
 	system("pause");
 	return 0;
 }
