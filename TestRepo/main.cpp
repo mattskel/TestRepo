e6c659cd1c2e4dd0fd42590bf5ccfd19ffeda20f
mattskelley <matthew.skelley@tjm.com.au>
2017-09-21 13:42:03 +1000

    Init commit to alternative branch

--- TestRepo/main.cpp
+++ TestRepo/main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main() {
 	cout << "Hello World" << endl;
 	cout << "This is my first change to the project" << endl;
+	cout << "Alternative branch" << endl;
 	system("pause");
 	return 0;
 }
