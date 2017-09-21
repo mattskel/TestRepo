46d0189acdd9e3de02f29a43fcd60465da1f08e7
mattskelley <matthew.skelley@tjm.com.au>
2017-09-21 13:31:06 +1000

    First change

---
+++ TestRepo/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Hello World" << endl;
+	cout << "This is my first change to the project" << endl;
+	system("pause");
+	return 0;
+}
